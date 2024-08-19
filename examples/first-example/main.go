664a0a836769a091ea7dd199a1df1cd3faa91de3
MeloSofia <sofia.melo@estudante.ifro.edu.br>
2024-08-19 15:42:54 -0400

    Update

--- examples/first-example/main.go
+++ examples/first-example/main.go
@@ -1,30 +1,65 @@
+// package main
+
+// import (
+// 	"log"
+
+// 	"github.com/hajimehoshi/ebiten/v2"
+// 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+// )
+
+// type Game struct{}
+
+// func (g *Game) Update() error {
+// 	return nil
+// }
+
+// func (g *Game) Draw(screen *ebiten.Image) {
+// 	ebitenutil.DebugPrint(screen, "Hello, World!")
+// }
+
+// func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+// 	return 320, 240
+// }
+
+// func main() {
+// 	ebiten.SetWindowSize(640, 480)
+// 	ebiten.SetWindowTitle("Hello, World!")
+// 	if err := ebiten.RunGame(&Game{}); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+// HLine draws a horizontal line
+
 package main
 
 import (
-	"log"
-
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+    "github.com/fogleman/gg"
 )
 
-type Game struct{}
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
-}
-
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
-		log.Fatal(err)
-	}
+    // Define o tamanho da imagem
+    const width = 500
+    const height = 500
+
+    // Cria um novo contexto de desenho
+    dc := gg.NewContext(width, height)
+
+    // Define a cor de fundo como branco
+    dc.SetRGB(1, 1, 1)
+    dc.Clear()
+
+    // Define a cor do quadrado como vermelho
+    dc.SetRGB(1, 0, 0)
+
+    // Desenha um quadrado no centro da imagem
+    size := 200.0
+    x := (width - size) / 2
+    y := (height - size) / 2
+    dc.DrawRectangle(x, y, size, size)
+
+    // Preenche o quadrado com a cor definida
+    dc.Fill()
+
+    // Salva a imagem em um arquivo PNG
+    dc.SavePNG("square.png")
 }
--- main.go
+++ main.go
@@ -1,4 +1,62 @@
 package main
-func main (){
-	
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"image/color"
+)
+
+const (
+	direita uint8 = 0
+	esquerda uint8 = 1
+)
+
+type Game struct{
+	tamQuadrado float64
+	posicaoDoQuadrado float64
+	tamTela float64
+	direcao uint8
+}
+
+func (g *Game) Update() error {
+	if g.direcao == direita {
+		if (g.posicaoDoQuadrado + g.tamQuadrado) < g.tamTela {
+			g.posicaoDoQuadrado += 5
+		
+		} else {
+			g.direcao = esquerda
+		}
+	} else if g.direcao == esquerda {
+		if g.posicaoDoQuadrado > 0 {
+			g.posicaoDoQuadrado -= 5
+		} else {
+			g.direcao = direita
+		}
+	}
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Define a cor do quadrado
+	col := color.RGBA{255, 0, 127, 255} // Azul
+
+
+	// Desenha um quadrado (rect)
+	y:= 100  // Coordenadas do canto superior esquerdo
+	ebitenutil.DrawRect(screen, g.posicaoDoQuadrado, float64(y), g.tamQuadrado, g.tamQuadrado, col)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return 640, 480
+}
+
+func main() {
+	game := &Game{posicaoDoQuadrado:0, tamTela: 640.0, direcao: direita, tamQuadrado: 200.0}
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetTPS(60)
+	ebiten.SetWindowTitle("Quadrado com BitEngine")
+	if err := ebiten.RunGame(game); err != nil {
+		panic(err)
+	}
 }
